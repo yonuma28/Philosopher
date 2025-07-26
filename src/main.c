4e4798d18da820b3b8ea685d9f03422156fea404
yukito-onuma <onuma.y.473@ms.saitama-u.ac.jp>
2025-07-26 20:41:05 +0900

    init

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yonuma <yonuma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 11:26:22 by marvin            #+#    #+#             */
-/*   Updated: 2025/07/26 18:57:04 by yonuma           ###   ########.fr       */
+/*   Updated: 2025/07/26 20:28:38 by yonuma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,8 @@ int main(int args, char *argv[])
     t_philo     *philos;
     t_mtx       *forks;
 
-    (void)args;
-    if (args != 5 || args != 6)
-        return (1);
+    if (!(args == 5 || args == 6))
+        return (write(STDERR_FILENO, "usage: 5 900 300 300 5\n", 23), 1);
     check_input(argv);
     if (init_program(&info, &philos, &forks, argv) == 1)
         return (clean_all(&info, philos, forks), 1);
